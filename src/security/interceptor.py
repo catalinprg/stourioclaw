b71ece1347ddf52e4a28b6dd694e4af5582df3da
catalin stour <catalinstour@catalins-MacBook-Air.local>
2026-03-19 15:29:12 +0200

    refactor: move browser_action to EXTERNAL_RISK_TOOLS, remove default agents except cybersecurity

--- src/security/interceptor.py
+++ src/security/interceptor.py
@@ -4,8 +4,8 @@ import re
 from dataclasses import dataclass, field
 from typing import Any, Dict, Optional
 
-HIGH_RISK_TOOLS = {"write_file", "execute_code", "browser_action"}
-EXTERNAL_RISK_TOOLS = {"call_api", "send_notification", "delegate_to_agent"}
+HIGH_RISK_TOOLS = {"write_file", "execute_code"}
+EXTERNAL_RISK_TOOLS = {"call_api", "send_notification", "delegate_to_agent", "browser_action"}
 SENSITIVE_PATTERNS = [
     re.compile(r"api[_-]?key", re.IGNORECASE),
     re.compile(r"secret", re.IGNORECASE),
